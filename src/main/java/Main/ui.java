2e061b0cde688f47c67a25c3a4e73ca445d92d7d
Endre Granerud Holmboe <128522447+outern88@users.noreply.github.com>
2023-05-03 14:12:53 +0200

    Fixed some stuff and added new scene for the "card" section.

--- src/main/java/Main/ui.java
+++ src/main/java/Main/ui.java
@@ -1,14 +1,12 @@
 package Main;
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import org.controlsfx.control.Notifications;
 
@@ -19,20 +17,10 @@ public class ui extends Application {
     @Override
     public void start(Stage primaryStage) {
         root = new BorderPane();
-        Scene scene = new Scene(root, 800, 600);
+        Scene mainScene = new Scene(root, 800, 600);
 
-        // Create menu bar and menu buttons
-        MenuBar menuBar = new MenuBar();
-        Menu fileMenu = new Menu("File");
-        MenuItem newMenuItem = new MenuItem("New");
-        MenuItem openMenuItem = new MenuItem("Open");
-        MenuItem saveMenuItem = new MenuItem("Save");
-        MenuItem exitMenuItem = new MenuItem("Exit");
-        exitMenuItem.setOnAction(event -> primaryStage.close()); // set action to close window
-        fileMenu.getItems().addAll(newMenuItem, openMenuItem, saveMenuItem, exitMenuItem);
-        menuBar.getMenus().addAll(fileMenu);
-
-        // Add menu bar to the top of the border pane
+        // Create menu bar and menu buttons for the main scene
+        MenuBar menuBar = createMenuBar(primaryStage);
         root.setTop(menuBar);
 
         // Create menu buttons for the left side of the menu
@@ -42,9 +30,14 @@ public class ui extends Application {
         Button boosterPacksButton = new Button("Booster Packs");
         Button miscButton = new Button("Misc");
 
+        // Add EventHandler for "kort" button
+        kortButton.setOnAction(event -> openNewScene(primaryStage, mainScene));
+
         // Add buttons to a VBox
-        VBox menuButtonBox = new VBox(10, kortButton, etbButton, collectionBoxButton, boosterPacksButton, miscButton);
+        VBox menuButtonBox = new VBox();
+        menuButtonBox.setSpacing(10);
         menuButtonBox.setPadding(new Insets(20, 10, 10, 10));
+        menuButtonBox.getChildren().addAll(kortButton, etbButton, collectionBoxButton, boosterPacksButton, miscButton);
 
         // Add VBox to a StackPane
         StackPane menuButtonPane = new StackPane(menuButtonBox);
@@ -62,8 +55,46 @@ public class ui extends Application {
 
         // Set window title and show the window
         primaryStage.setTitle("My Collection");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(mainScene);
         primaryStage.show();
+    }
+
+    private void openNewScene(Stage primaryStage, Scene mainScene) {
+        // Create the new scene's root layout
+        VBox newSceneRoot = new VBox();
+        newSceneRoot.setSpacing(10);
+        newSceneRoot.setPadding(new Insets(10));
+
+        // Add the menu bar to the new scene
+        MenuBar newSceneMenuBar = createMenuBar(primaryStage);
+        newSceneRoot.getChildren().add(0, newSceneMenuBar);
+
+        Label newSceneLabel = new Label("This is the new scene.");
+        newSceneRoot.getChildren().add(newSceneLabel);
+
+        // Create the new scene and set it to the stage
+        Scene newScene = new Scene(newSceneRoot, 800, 600);
+
+        // Create a back button to return to the main scene
+        Button backButton = new Button("Back");
+        backButton.setOnAction(event -> primaryStage.setScene(mainScene));
+        newSceneRoot.getChildren().add(backButton);
+
+        // Set the new scene to the stage
+        primaryStage.setScene(newScene);
+    }
+    private MenuBar createMenuBar(Stage primaryStage) {
+        MenuBar menuBar = new MenuBar();
+        Menu fileMenu = new Menu("File");
+        MenuItem newMenuItem = new MenuItem("New");
+        MenuItem openMenuItem = new MenuItem("Open");
+        MenuItem saveMenuItem = new MenuItem("Save");
+        MenuItem exitMenuItem = new MenuItem("Exit");
+        exitMenuItem.setOnAction(event -> primaryStage.close()); // set action to close window
+        fileMenu.getItems().addAll(newMenuItem, openMenuItem, saveMenuItem, exitMenuItem);
+        menuBar.getMenus().addAll(fileMenu);
+
+        return menuBar;
     }
 
     public static void main(String[] args) {
